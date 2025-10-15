82ba279aa8c14eae9515140ef84f99d869fda961
Dani Rolam <dani.rolam@gmail.com>
2025-10-14 22:57:59 -0400

    initial commit

---
+++ my-portfolio/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">Dani Rolam</h1>
+      <p className="mt-2 text-lg">Portfolio coming soon...</p>
+    </main>
+  );
+}
