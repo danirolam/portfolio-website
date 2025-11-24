79e0089db3d1375d16728807c8d23288cf839535
Dani Rolam <dani.rolam@gmail.com>
2025-11-24 12:14:50 -0500

    refactor: move app to root

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold">Dani Rolam</h1>
+      <h1 className="text-4xl font-bold"></h1>
       <p className="mt-2 text-lg">Portfolio coming soon...</p>
     </main>
   );
